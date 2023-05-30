faaa6a1d2146fb4948027106c61f69eb5ff4b8ac
bagoviggo <bagoviggo@gmail.com>
2023-05-31 01:45:33 +0300

    Add task 5

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,17 +9,29 @@ from flask import (
 from os import environ
 from models import storage
 from api.v1.views import app_views
+from flask_cors import CORS
 
 
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
-
+cors = CORS(app, resources={r'/*': {'origins': '0.0.0.0'}})
 
 @app.teardown_appcontext
 def close_db(error):
     '''Close the database'''
     storage.close()
+
+@app.errorhandler(404)
+def not_found(error):
+    ''' 404 Error
+    ---
+    responses:
+      404:
+        description: a resource was not found
+    '''
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
 
 
 if __name__ == '__main__':
