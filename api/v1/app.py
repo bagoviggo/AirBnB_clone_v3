887e5961c51a71c4818b762b6e42f83de9ae707f
bagoviggo <bagoviggo@gmail.com>
2023-05-31 03:14:51 +0300

    Add Documentation

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-''' Flask Application '''
+""" Flask Application """
 from flask import (
     Flask,
     jsonify,
@@ -13,33 +13,33 @@ from flask_cors import CORS
 
 
 app = Flask(__name__)
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+app.config["JSONIFY_PRETTYPRINT_REGULAR"] = True
 app.register_blueprint(app_views)
-cors = CORS(app, resources={r'/*': {'origins': '0.0.0.0'}})
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 @app.teardown_appcontext
 def close_db(error):
-    '''Close the database'''
+    """Close the database"""
     storage.close()
 
 @app.errorhandler(404)
 def not_found(error):
-    ''' 404 Error
+    """ 404 Error
     ---
     responses:
       404:
         description: a resource was not found
-    '''
-    return make_response(jsonify({'error': 'Not found'}), 404)
+    """
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 
-if __name__ == '__main__':
-    ''' Main Function '''
-    host = environ.get('HBNB_API_HOST')
-    port = environ.get('HBNB_API_PORT')
+if __name__ == "__main__":
+    """ Main Function """
+    host = environ.get("HBNB_API_HOST")
+    port = environ.get("HBNB_API_PORT")
     if not host:
-        host = '0.0.0.0'
+        host = "0.0.0.0"
     if not port:
-        port = '5000'
+        port = "5000"
     app.run(host=host, port=port, threaded=True)
